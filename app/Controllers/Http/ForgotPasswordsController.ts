4cc540b64de8adfbd6159311f495b8c5d77487a3
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-08-03 12:43:57 -0300

    Email sending

--- app/Controllers/Http/ForgotPasswordsController.ts
+++ app/Controllers/Http/ForgotPasswordsController.ts
@@ -1,11 +1,12 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
+import Mail from '@ioc:Adonis/Addons/Mail'
 import { string } from '@ioc:Adonis/Core/Helpers'
 
 export default class ForgotPasswordsController {
   public async store({ request, response }: HttpContextContract) {
     try {
-      const email = request.input('email')
+      const { email, redirect_url: url } = request.only(['email', 'redirect_url'])
 
       const user = await User.findByOrFail('email', email)
 
@@ -14,7 +15,13 @@ export default class ForgotPasswordsController {
 
       await user.save()
 
-      return user
+      await Mail.sendLater((message) => {
+        message
+          .from('d@v.i')
+          .to(email)
+          .subject('Recuperação de senha!')
+          .htmlView('emails/forgot_password', { email, token: user.token, url })
+      })
     } catch (e) {
       return response.status(e.status).send({
         error: {
